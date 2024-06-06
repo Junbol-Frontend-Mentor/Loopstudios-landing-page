cf7ab6a88871aef313280ea9d2eb4bd4b16976fc
Junier Bolivar <junierba@yahoo.co.uk>
2024-06-06 19:43:54 +0200

    fix grid

--- scripts/script.js
+++ scripts/script.js
@@ -4,8 +4,9 @@ document.addEventListener('DOMContentLoaded', function () {
   let myHamburgerIcon = document.getElementById('hamburgerIcon');
   let myNavMenu = document.getElementById('navigationMenu');
   let mySlogan = document.getElementById('sloganId');
-  let myExtraCards = document.getElementById('extraCardsId');
   let mySeeAll = document.getElementById('seeAllId');
+  let creationsGridContainer = document.querySelector('.creationsGridContainer');
+  let extraCards = document.querySelectorAll('.creationsGridContainer__card.hide');
 
   myHamburgerIcon.addEventListener('click', function () {
     myHamburgerIcon.classList.toggle('change');
@@ -14,6 +15,10 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
   mySeeAll.addEventListener('click', function () {
-    myExtraCards.classList.toggle('creationsContainer__extraCards--visible');
+    extraCards.forEach(card => {
+      card.classList.toggle('hide');//Each extra card's hide class is toggled. If the card is hidden, it becomes visible, and vice versa.
+    });
+    creationsGridContainer.classList.toggle('expanded'); // 🟢 Toggle (Remove or Add) the expanded class
+    creationsGridContainer.classList.toggle('collapsed'); // 🟢 Toggle (Remove or Add) the collapsed class
   });
 });
